bad60a4a377a04f0d0137e30c9de2855c3eaccd7
pranali <pranaligawade1369@gmail.com>
2021-07-23 00:26:55 +0530

    hiii commited

---
+++ JOBPORTAL1/Admin/footer.php
@@ -0,0 +1,10 @@
+   <!-- Footer -->
+    <div id="footer">
+        
+        <hr class="noscreen" />
+        
+        <p class="style1" id="createdby">
+          <!-- DON´T REMOVE, PLEASE! -->
+        </p>
+        <p id="copyright">&copy; 2019, Developed By Pranali and Samina</p>
+  </div> <!-- /footer -->
---
+++ JOBPORTAL1/Employer/right.php
@@ -0,0 +1,48 @@
+        <!-- Right column -->
+<?php
+
+if(!isset($_SESSION))
+{
+session_start();
+}
+?>
+
+<div id="col" class="noprint">
+            <div id="col-in">
+
+                <!-- About Me -->
+                <h3>Welcome</h3>
+
+                <div id="about-me">
+                    <p>
+                    <?php 
+					//echo "mohan";
+					echo $_SESSION['Name'];
+					?>
+                    </p>
+              </div> <!-- /about-me -->
+
+                <hr class="noscreen" />
+
+                <!-- Category -->
+                <h3 >&nbsp;</h3>
+
+              <br/>
+
+      <hr class="noscreen" />
+
+                <!-- Archive -->
+                <h3>&nbsp;</h3>
+
+                <br/>
+
+            <hr class="noscreen" />
+
+                <!-- Links -->
+                <h3>&nbsp;</h3>
+<br/>
+
+<hr class="noscreen" />
+          </div> <!-- /col-in -->
+</div> 
+<!-- /col -->
